f97b085f075d05a440ddbf3e09ab2d95f55a9c99
Abdullah Khan Kakar <2022bbit71@student.uet.edu.pk>
2025-10-21 23:12:37 +0500

    execute Raw SQL Queries

--- lib/helper/database_helper.dart
+++ lib/helper/database_helper.dart
@@ -1,7 +1,82 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DatabaseHelper {
   DatabaseHelper._privateConstructor();
 
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
-  String name = "A";
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      print("Database already exist at path: ${_database!.path}");
+      return _database!;
+    }
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'my_database.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        print("Database successfully created at path: ${db.path}");
+        // Create tables here
+        db.execute('''
+        CREATE TABLE student (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          age INTEGER,
+          address TEXT,
+          enrollment_date TEXT
+        )
+        ''');
+      },
+    );
+  }
+
+  Future<void> insertValueIntoStudentTable() async {
+    _database ??= await _initDatabase();
+    await _database!.transaction((txn) async {
+      int id1 = await txn.rawInsert(
+        'INSERT INTO Student (name, age, address, enrollment_date) VALUES ("Jane Smith", 22, "456 Elm St", "2023-09-15")',
+      );
+      print("Inserted row id: $id1");
+      int id2 = await txn.rawInsert(
+        'INSERT INTO Student (name, age, address, enrollment_date) VALUES ("Alice Johnson", 19, "789 Oak St", "2023-08-20")',
+      );
+      print("Inserted row id: $id2");
+    });
+  }
+
+  Future<void> getAllStudentRecords() async {
+    _database ??= await _initDatabase();
+    List<Map<String, dynamic>> records = await _database!.rawQuery(
+      'SELECT * FROM Student',
+    );
+    print("Student Records: $records");
+  }
+
+  Future<void> updateStudentRecordById(int id) async {
+    _database ??= await _initDatabase();
+
+    int changesCount = await _database!.rawUpdate(
+      'UPDATE Student SET name = "Abdullah Khan Kakar", address = "Lahore, PAKISTAN"',
+    );
+    print("Changes Count: $changesCount");
+  }
+
+  Future<void> deleteAllStudentRecords() async {
+    _database ??= await _initDatabase();
+
+    int changesCount = await _database!.rawDelete('DELETE FROM Student');
+    print("Changes Count: $changesCount");
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:explore_sqflite/helper/database_helper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,8 +37,67 @@ class _HomePageState extends State<HomePage> {
           children: [
             Text('Initialize Database'),
             SizedBox(height: 10),
-            ElevatedButton(onPressed: () {}, child: Text("Click Here")),
+            ElevatedButton(
+              onPressed: () async {
+                var db = await DatabaseHelper.instance.database;
+              },
+              child: Text("Click Here"),
+            ),
             SizedBox(height: 20),
+
+            // Raw SQL Queries
+            Text(
+              'Raw SQL Queries',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text('Insert Values Into Table'),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                DatabaseHelper db = DatabaseHelper.instance;
+                db.insertValueIntoStudentTable();
+              },
+              child: Text("Click Here"),
+            ),
+            SizedBox(height: 20),
+            Text('Get Student Records'),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                DatabaseHelper db = DatabaseHelper.instance;
+                db.getAllStudentRecords();
+              },
+              child: Text("Click Here"),
+            ),
+            SizedBox(height: 20),
+            Text('Update Student Record At ID'),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                DatabaseHelper db = DatabaseHelper.instance;
+                db.updateStudentRecordById(5);
+              },
+              child: Text("Click Here"),
+            ),
+            SizedBox(height: 20),
+            Text('Delete All Student Records'),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                DatabaseHelper db = DatabaseHelper.instance;
+                db.deleteAllStudentRecords();
+              },
+              child: Text("Click Here"),
+            ),
+
+            // SQL Helpers
+            SizedBox(height: 20),
+            Text(
+              'SQL Helper Methods',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
           ],
         ),
       ),
